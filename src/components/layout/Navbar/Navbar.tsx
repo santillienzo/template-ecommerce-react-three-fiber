9fad58de0b4bbc79d98a63e5958d1917b1507fc3
santillienzo <enzo135246@gmail.com>
2024-01-20 04:12:26 -0300

    feat: add svg menu icon

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,18 +1,39 @@
 import style from './Navbar.module.css'
+import menu_restaurante from '../../../assets/menu-icons/restaurante_menu.svg'
+import menu from '../../../assets/menu-icons/menu.svg'
+import { useState } from 'react'
+
+
 
 const Navbar = () => {
+    const [menuIsOpen, setMenuIsOpen] = useState<boolean>(true)
+
+    const toggleMenu = ()=> setMenuIsOpen(!menuIsOpen)
+
   return (
-    <div className={style.container}>
-        <h1 className={style.logo}>Logo</h1>
-        <div className={style.categories_container}>
-            <ul>
-                <li><span>Categoría 1</span></li>
-                <li><span>Categoría 2</span></li>
-                <li><span>Categoría 3</span></li>
-                <li><span>Categoría 4</span></li>
-            </ul>
+    <header className={style.container}>
+        <div className={style.menu_icon_container}>
+            <div className={style.menu_icon} onClick={toggleMenu}>
+                {
+                    menuIsOpen ?
+                    <img src={menu_restaurante} alt="" />
+                    :
+                    <img src={menu} alt=""/>
+                }
+            </div>
         </div>
-    </div>
+        <nav className={`${style.nav} animate__animated animate__slideInDown`}>
+            <h1 className={style.logo}>Logo</h1>
+            <div className={style.categories_container}>
+                <ul>
+                    <li><span>Categoría 1</span></li>
+                    <li><span>Categoría 2</span></li>
+                    <li><span>Categoría 3</span></li>
+                    <li><span>Categoría 4</span></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
   )
 }
 
